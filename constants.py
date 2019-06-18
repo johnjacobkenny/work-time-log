6a6efd19e57aed4ab937247cdd19309ef7e0edb6
Kenny John Jacob <johnjacobkenny@gmail.com>
2019-06-18 22:11:35 +0530

    feat(.env): Enable reading of .env file

--- constants.py
+++ constants.py
@@ -1,4 +1,11 @@
+import os
 from os import path
+from dotenv import load_dotenv
+load_dotenv()
 
-LOG_FILENAME = "log.csv"
+LOG_FILENAME = os.getenv("LOG_FILENAME")
 LOG_FILEPATH = path.join(path.dirname(__file__), LOG_FILENAME)
+
+filepath = os.getenv("LOG_FILEPATH")
+if filepath:
+    LOG_FILEPATH = path.join(filepath, LOG_FILENAME)
