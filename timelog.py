6350e5ef7a2912fa964d34946e613122a5b58513
Kenny John Jacob <johnjacobkenny@gmail.com>
2019-06-19 21:45:08 +0530

    fix: Assignment to copy issue
    Understood that we should use iat or at for assignment to a single cell
    in a df

--- timelog.py
+++ timelog.py
@@ -4,12 +4,12 @@ import click
 now = pd.to_datetime("today")
 
 def clock(data):
-    isClockingIn = not pd.isnull(data.exit.iloc[-1])
+    isClockingIn = not pd.isnull(data.iat[-1, -1])
     if isClockingIn:
         click.secho("Clocking in", bold = True, fg="green")
         data = data.append(
             {"date": now.date(), "enter": now}, ignore_index=True)
     else:
         click.secho("Clocking out", bold = True, fg="red")
-        data.exit.iloc[-1] = now
+        data.iat[-1, -1] = now
     return data
